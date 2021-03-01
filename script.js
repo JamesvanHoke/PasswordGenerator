751a8432387091795ec01292609b6027eaac735e
James V <james.vanhoke@yahoo.com>
2021-02-28 18:09:43 -0700

    Fixed Password Characters array generation

--- script.js
+++ script.js
@@ -90,13 +90,14 @@ var upperCasedCharacters = [
   "Z",
 ];
 
+//Creates blank variable for the user prompt to feed the desired characters into
 var PasswordCharacters = "";
+
+//Creates blank variable for the password to be fed into
+var Password = "";
 
 //Retrieves reference to to the button with the generate ID
 var generateBtn = document.querySelector("#generate");
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
   //Prompt User for Password length
@@ -110,56 +111,47 @@ function generatePassword() {
   }
   //Confirm prompt user if they want Special Characters
   var UseSpecial = confirm("Do you want Special Characters? (!,#,?, etc)");
+  //Adds Special characters to password generation
+  if (UseSpecial) {
+    PasswordCharacters = [...PasswordCharacters, ...specialCharacters]
+  }
   //Confirm prompt user if they want Numeric Characters
   var UseNumeric = confirm("Do you want Numeric Characters? (0-9)");
+  //Adds Numeric characters to password generation
+  if (UseNumeric) {
+    PasswordCharacters = [...PasswordCharacters, ...numericCharacters];
+    // PasswordCharacters += numericCharacters;
+  }
   //Confirm prompt user if they want uppercase Characters
   var UseUppercase = confirm("Do you want Uppercase Characters?  (A-Z)");
+  //Adds Upper Case characters to password generation
+  if (UseUppercase) {
+    PasswordCharacters = [...PasswordCharacters, ...upperCasedCharacters];
+  }
   //Confirm prompt user if they want lowercase Characters
   var UseLowercase = confirm("Do you want Lowercase Characters? (a-z)");
-
+  //Adds Lower Case characters to password generation
+  if (UseLowercase) {
+    PasswordCharacters = [...PasswordCharacters, ...lowerCasedCharacters];
+  }
   //Checks to make sure one of the character sets is used.
   if (!UseLowercase && !UseNumeric && !UseSpecial && !UseUppercase) {
     return alert("At least one character type must be selected");
   }
-  //Adds special characters to password generation
-  if (UseSpecial) {
-    PasswordCharacters += specialCharacters;
-  }
-  //Adds numeric characters to password generation
-  if (UseNumeric) {
-    PasswordCharacters += numericCharacters;
-  }
-  //Adds Upper Case characters to password generation
-  if (UseUppercase) {
-    PasswordCharacters += upperCasedCharacters;
-  }
-  //Adds Lower Case characters to password generation
-  if (UseLowercase) {
-    PasswordCharacters += lowerCasedCharacters;
-  }
 
-  // return the build password
-  return "Hello I am the password place holder :)";
+  //For loops iterates based on Password length specified by user. Uses math.random to randomly select an index from our generated PasswordCharacters array, and continues until we reach desired password length
+  for (var i = 0; i < PasswordLength; i++) {
+    password +=
+      PasswordCharacters[Math.floor(Math.random() * PasswordCharacters.length)];
+  }
+  return password;
 }
 
-// Retrieve a random item from the provided array
-function getRandomItem(arr) {
-  // Generate a random index from 0 to the length - 1 of our array
-  var randomIndex = Math.random() * arr.length;
-  // round down our random index
-  randomIndex = Math.floor(randomIndex);
-  // return the random item based off of our random index
-  return arr[randomIndex];
-  // One liner of the above code
-  // return arr[Math.floor(Math.random() * arr.length)];
-}
-
-// console.log(getRandomItem(ArrayName));
-
-// Write password to the #password input
+//Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 }
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
